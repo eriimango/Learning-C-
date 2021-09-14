ba96af2ac527fdbcc01634425c8f43e013284a1a
Ericka <64378078+eriimango@users.noreply.github.com>
2021-09-13 23:58:14 -0400

    Create SimpleC.cpp

---
+++ SimpleC.cpp
@@ -0,0 +1,26 @@
+/**This is the begining of assignment 1
+output objective assignment
+Authur:Ericka H.
+*/
+//this is how you set up a simple c program.
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string first_line;
+    string middle_line;
+    first_line = "ccccccccc\t\t++\t\t\t++\n";
+    middle_line = "cc\t\t  ++++++++++++++\t  ++++++++++++++\n";
+
+    cout << first_line;
+    cout << "cc\t\t\t++\t\t\t++\n";
+    cout << middle_line;
+    cout << middle_line;
+    cout << "cc\t\t\t++\t\t\t++\n";
+    cout << first_line;
+
+    return 0;
+
+}
