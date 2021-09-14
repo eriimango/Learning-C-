0e1e7633370fdd0f266153d0f155cc0c09d4a831
Ericka <64378078+eriimango@users.noreply.github.com>
2021-09-14 00:00:38 -0400

    Create Integer_Pyramid.cpp

---
+++ Integer_Pyramid.cpp
@@ -0,0 +1,19 @@
+// this program displays an integers pyramid 
+// author: Ericka H.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    cout << "1                           1 \n";
+    cout << "1 2                       2 1 \n";
+    cout << "1 2 3                   3 2 1 \n";
+    cout << "1 2 3 4               4 3 2 1 \n";
+    cout << "1 2 3 4 5           5 4 3 2 1 \n";
+    cout << "1 2 3 4 5 6       6 5 4 3 2 1 \n";
+    cout << "1 2 3 4 5 6 7   7 6 5 4 3 2 1 \n";
+    cout << "1 2 3 4 5 6 7 8 7 6 5 4 3 2 1 \n";
+
+    return 0;
+}
